14119edef7ccfa142486b46390a4fec93b3d50dd
jyothirmayee564 <94290151+jyothirmayee564@users.noreply.github.com>
2023-06-05 22:18:54 +0530

    creating and updating the user card 2

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // alert("Hi,Alert Message");
 console.log("Hi,Alert Message");
+
+var data1={image : "recentPic.jpeg",name : "Jyothirmayee Kandiboina", designation:"Student"};
+var data2={image : "abc.jpg",name : "Jyothi", designation:"Intern"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata= data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("myname").innerHTML=displaydata.name;
+    document.getElementById("mydesig").innerHTML=displaydata.designation;
+}
